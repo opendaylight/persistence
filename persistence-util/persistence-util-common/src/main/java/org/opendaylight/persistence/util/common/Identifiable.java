91230da353c42492cc80e52f43daafd85c3c6ecc
Nachiket Abhyankar <nabhyank@hp.com>
2015-02-05 06:39:21 -0800

    Adding Identifiable interface along with a few common utility classes to Persistence
    Change-Id: I9e45a7a133408f80fa592dea34be5afa257f04df
    Signed-off-by: Nachiket Abhyankar <nabhyank@hp.com>

---
+++ persistence-util/persistence-util-common/src/main/java/org/opendaylight/persistence/util/common/Identifiable.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2015 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.persistence.util.common;
+
+import org.opendaylight.persistence.util.common.type.Id;
+
+import java.io.Serializable;
+
+/**
+ * Identifiable.
+ *
+ * @param <T> type of the identified object
+ * @param <I> type of the id. This type should be immutable and it is critical it implements
+ *            {@link Object#equals(Object)} and {@link Object#hashCode()} correctly.
+ * @author Fabiel Zuniga
+ * @author Nachiket Abhyankar
+ */
+public interface Identifiable<T, I extends Serializable> {
+
+    /**
+     * Gets the id of this object.
+     * <P>
+     * A type for the identified must be specified to retrieve the id because identified objects
+     * might be inheritable. For example, assume {@code Employee} extends from {@code Person} and
+     * {@code Person} implements {@code Identifiable<Person, Long>}. The following code would be
+     * possible.
+     *
+     * <pre>
+     * Id&lt;Person, Long&gt; id = employee.getId();
+     * Id&lt;Employee, Long&gt; id = employee.getId();
+     *
+     * <pre>
+     *
+     * @return the id of this object
+     */
+    public <E extends T> Id<E, I> getId();
+}
